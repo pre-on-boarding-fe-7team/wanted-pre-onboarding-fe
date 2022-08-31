fdbf9ce0972440f822ce8293962d1a07347ad16b
Malza0408 <parkjh04080@gmail.com>
2022-08-31 21:31:46 +0900

    Feat: NOT FOUND page Router에 추가

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { ROUTE } from '../common/utils/constant';
 import MainPage from '../pages/Main';
 import TodoPage from '../pages/Todo';
+import NotFoundPage from '../pages/NotFound';
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path={ROUTE.MAIN} element={<MainPage />} />
         <Route path={ROUTE.TODO} element={<TodoPage />} />
+        <Route path={ROUTE.NOT_FOUND} element={<NotFoundPage />} />
       </Routes>
     </>
   );

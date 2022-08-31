1e3468dfa24db3f226173d499417a2bc8c224985
Malza0408 <parkjh04080@gmail.com>
2022-08-31 20:49:06 +0900

    Feat: Main Component

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE } from '../../common/utils/constant';
+import AuthForm from '../../components/Auth/AuthForm';
+import { Container, Form, SwitchButton, Title } from './index.style';
+
+function Main() {
+  const navigate = useNavigate();
+  const [isLoginPage, setIsLoginPage] = useState(true);
+
+  const handleSetIsLoginPage = isLogin => {
+    setIsLoginPage(isLogin);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      navigate(ROUTE.TODO);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Title>Welcome To Wanted pre onboarding Todo List</Title>
+      <Form>
+        {isLoginPage ? (
+          <SwitchButton onClick={() => setIsLoginPage(false)}>회원가입 페이지로 이동</SwitchButton>
+        ) : (
+          <SwitchButton onClick={() => setIsLoginPage(true)}>로그인 페이지로 이동</SwitchButton>
+        )}
+        <AuthForm isLoginPage={isLoginPage} handleSetIsLoginPage={handleSetIsLoginPage} />
+      </Form>
+    </Container>
+  );
+}
+
+export default Main;

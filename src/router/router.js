82c7f79cc379aeb308a19f2c870bf475103ae6b0
Malza0408 <parkjh04080@gmail.com>
2022-08-31 21:16:19 +0900

    Feat: Todo page

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { ROUTE } from '../common/utils/constant';
 import MainPage from '../pages/Main';
+import TodoPage from '../pages/Todo';
 
 function Router() {
   return (
     <>
       <Routes>
         <Route path={ROUTE.MAIN} element={<MainPage />} />
+        <Route path={ROUTE.TODO} element={<TodoPage />} />
       </Routes>
     </>
   );

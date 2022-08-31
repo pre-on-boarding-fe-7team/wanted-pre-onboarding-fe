0d4a37c980c93b1247c1af19259a3d0782e4dc46
Malza0408 <parkjh04080@gmail.com>
2022-08-31 18:41:43 +0900

    Feat: route 경로 상수화

---
+++ src/common/utils/constant.js
@@ -0,0 +1,12 @@
+export const ROUTE = {
+  MAIN: '/',
+  TODO: '/todo',
+};
+
+export const API_URL = {
+  AUTH: {
+    SIGN_UP: '/auth/signup',
+    SIGN_IN: '/auth/signin',
+  },
+  TODO: '/todos',
+};
